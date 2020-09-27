e4853afa96eea087c000ff036a0bcccee196fdb8
Suraj Yadav <surajyadav20@outlook.com>
2020-09-27 18:36:21 +0530

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,6 @@ var logger = Logger(
 
 Directory CACHE_DIRECTORY;
 
-Iterable<List<T>> zip<T>(Iterable<Iterable<T>> iterables) sync* {
-  if (iterables.isEmpty) return;
-  final iterators = iterables.map((e) => e.iterator).toList(growable: false);
-  while (iterators.every((e) => e.moveNext())) {
-    yield iterators.map((e) => e.current).toList(growable: false);
-  }
-}
-
 void main() async {
   final shortcut = Map.of(WidgetsApp.defaultShortcuts)
     ..remove(LogicalKeySet(LogicalKeyboardKey.escape))
