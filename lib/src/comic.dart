8eee2f09161a0f349131bc220a19d3f27c659cb0
Suraj Yadav <surajyadav20@outlook.com>
2023-04-27 23:41:12 +0530

    Integrate libarchive for RAR (#10)
    * Remove ios
    
    * Integrated libarchive windows
    
    * Update build step
    
    * Fix script path
    
    * Add build target
    
    * Fix permission issues
    
    * Build libarchive for macos
    
    * See all generated files
    
    * Change dylib paths
    
    * Fix path
    
    * Use dylib from brew
    
    * Handle M1 mac
    
    * Use package for libarchive
    
    * Logs
    
    * clone libarchive in podspec
    
    * SSH into github action
    
    * build framework using cmake
    
    * Remove Macos Build
    
    * Fix test case

--- lib/src/comic.dart
+++ lib/src/comic.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
-import 'package:archive/archive_io.dart';
-import 'package:collection/collection.dart';
 import 'package:comic_reader/main.dart';
+import 'package:archive/archive.dart' as archive;
 import 'package:flutter/material.dart';
 import 'package:image_size_getter/file_input.dart' as image_size_getter;
 import 'package:image_size_getter/image_size_getter.dart' as image_size_getter;
-import 'package:mime/mime.dart';
+import 'package:mime/mime.dart' as mime;
 import 'package:path/path.dart' as path;
 
 class ComicPage {
@@ -16,10 +15,8 @@ class ComicPage {
   ComicPage(this.imgFile, this.size);
 }
 
-ComicPage savePage(String filePath, ArchiveFile file) {
-  final outputStream = OutputFileStream(filePath);
-  file.writeContent(outputStream);
-  outputStream.close();
+ComicPage savePage(String filePath, archive.ArchiveFile file) {
+  file.writeContent(filePath);
 
   final imageFile = File(filePath);
 
@@ -39,29 +36,30 @@ class Comic {
 
   Comic({required this.archiveFilePath});
 
-  bool loadComicPreview() {
-    bool success = true;
-    final archive = ZipDecoder().decodeBuffer(InputFileStream(archiveFilePath));
-
+  void loadComicPreview() {
     final extractedCache = Directory(path.join(
         cacheDirectory.path, path.basenameWithoutExtension(archiveFilePath)));
 
-    numberOfPages = archive.files.length;
+    ComicPage? cover;
 
-    archive.files.sort((a, b) => compareAsciiLowerCaseNatural(a.name, b.name));
-
-    for (var file in archive.files) {
+    var pageCount = 0;
+    for (var file in archive.getArchiveFiles(archiveFilePath)) {
       if (!file.isFile) {
         continue;
       }
-      final mimeType = lookupMimeType(file.name, headerBytes: file.content);
+      final mimeType =
+          mime.lookupMimeType(file.path, headerBytes: file.headerBytes);
 
       if (mimeType != null && mimeType.startsWith("image/")) {
-        firstPage = savePage(
-            "${extractedCache.path}_Cover${path.extension(file.name)}", file);
-        break;
+        cover ??= savePage(
+            "${extractedCache.path}_Cover${path.extension(file.path)}", file);
+        pageCount++;
       }
     }
-    return success;
+    numberOfPages = pageCount;
+    if (cover == null) {
+      throw const FormatException("Archive had no image file");
+    }
+    firstPage = cover;
   }
 }
