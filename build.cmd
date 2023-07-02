cc85077c43ca31a863a8c95922c3d2b116ec8986
Suraj Yadav <surajyadav20@outlook.com>
2023-07-02 20:56:47 +0530

    Do not use ninja

--- build.cmd
+++ build.cmd
@@ -9,11 +9,11 @@ IF DEFINED VCPKG_INSTALLATION_ROOT (
     set CACHE=--binarysource=clear;x-gha,readwrite
 )
 
-vcpkg install --triplet x64-windows tweeny wxwidgets freeimage libarchive[core] %CACHE%
+vcpkg install --triplet x64-windows tweeny wxwidgets freeimage libarchive[core] benchmark gtest %CACHE%
 
 
-cmake -B build -S . -GNinja -DCMAKE_BUILD_TYPE=Release || rmdir /S /Q build && mkdir build 
-cmake -B build -S . -GNinja -DCMAKE_BUILD_TYPE=Release
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release || rmdir /S /Q build && mkdir build 
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake --build build --target package
