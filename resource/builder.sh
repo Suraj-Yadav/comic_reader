37d99e6921b8b767f4b125b8f1b94f9ce149d57f
Suraj Yadav <surajyadav20@outlook.com>
2025-04-20 05:27:33 +0000

    Add Appimage for linux (#15)
    * Add appimage builder
    
    * update version
    
    * Fixes

---
+++ resource/builder.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+export CMAKE_TOOLCHAIN_FILE=$VCPKG_INSTALLATION_ROOT/scripts/buildsystems/vcpkg.cmake
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build --target package --config Release
+
+pushd build
+FILE=$(ls comic_reader-*.zip)
+if [ "$RUNNER_OS" = "Linux" ]; then
+	rm -rf AppDir
+
+	export APPIMAGE_EXTRACT_AND_RUN=1
+	export NO_STRIP=true
+
+	NAME="${FILE%.*}"
+	unzip $FILE
+	mv $NAME AppDir
+
+	wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+	chmod +x linuxdeploy-*.AppImage
+	./linuxdeploy-*.AppImage --appdir AppDir --desktop-file ../resource/comic_reader.desktop --icon-file ../resource/comic_reader.png -e AppDir/bin/comic_reader --output appimage
+	mv Comic_Reader-x86_64.AppImage comic_reader.AppImage
+else
+	mv $FILE comic_reader-win64.zip
+fi
+popd
