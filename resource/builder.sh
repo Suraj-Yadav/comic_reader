62b19076fd2d0b01fcb21c4ebd3a4989af0990ba
Suraj Yadav <surajyadav20@outlook.com>
2025-07-06 01:09:07 +0000

    Add update info and vcpkg cache (#16)
    * Add update info and vcpkg cache
    
    * Fix path
    
    * Add review suggestion
    
    * Fix path again
    
    * Fix path for windows
    
    * use workspace for path
    
    * Only limit release step
    
    * fix upload

--- resource/builder.sh
+++ resource/builder.sh
@@ -4,7 +4,10 @@ set -e
 
 export CMAKE_TOOLCHAIN_FILE=$VCPKG_INSTALLATION_ROOT/scripts/buildsystems/vcpkg.cmake
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release || {
+	cat build/vcpkg_installed/vcpkg/issue_body.md
+	exit 1
+}
 
 cmake --build build --target package --config Release
 
@@ -15,15 +18,18 @@ if [ "$RUNNER_OS" = "Linux" ]; then
 
 	export APPIMAGE_EXTRACT_AND_RUN=1
 	export NO_STRIP=true
+	export UPDATE_INFORMATION='gh-releases-zsync|Suraj-Yadav|comic_reader|latest|Comic_Reader-*x86_64.AppImage.zsync'
 
 	NAME="${FILE%.*}"
 	unzip $FILE
 	mv $NAME AppDir
 
-	wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+	pushd /tmp
+	wget -c https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 	chmod +x linuxdeploy-*.AppImage
-	./linuxdeploy-*.AppImage --appdir AppDir --desktop-file ../resource/comic_reader.desktop --icon-file ../resource/comic_reader.png -e AppDir/bin/comic_reader --output appimage
-	mv Comic_Reader-x86_64.AppImage comic_reader.AppImage
+	popd
+
+	/tmp/linuxdeploy-*.AppImage --appdir AppDir --desktop-file ../resource/comic_reader.desktop --icon-file ../resource/comic_reader.png -e AppDir/bin/comic_reader --output appimage
 else
 	mv $FILE comic_reader-win64.zip
 fi
